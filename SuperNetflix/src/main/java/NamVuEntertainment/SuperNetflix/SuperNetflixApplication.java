ba877a825b03775d1217dd89a337c2fb6df40818
NamNguyen <namvannguyen.andy@gmail.com>
2020-11-13 10:41:23 +0700

    Initiate Project

---
+++ SuperNetflix/src/main/java/NamVuEntertainment/SuperNetflix/SuperNetflixApplication.java
@@ -0,0 +1,13 @@
+package NamVuEntertainment.SuperNetflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperNetflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperNetflixApplication.class, args);
+	}
+
+}
---
+++ SuperNetflix/src/test/java/NamVuEntertainment/SuperNetflix/SuperNetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package NamVuEntertainment.SuperNetflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperNetflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
